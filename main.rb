ea78b93230a5a2d4213dda70f41e6af68112c2b4
m-negishi <negileek22@gmail.com>
2013-02-26 10:19:24 +0900

    Initial Negi Commit

---
+++ main.rb
@@ -0,0 +1,18 @@
+require 'dxruby'
+
+#ã¡Ç´ç≈è¨ÇÃå¥ë•
+#player_img = Image.load("./images/player.png")
+#Window.loop do
+#	break if Input.keyPush?(K_ESCAPE)
+#	Window.draw(100, 100, player_img)
+#	Window.draw(640-32, 480-32, player_img)
+#	Window.draw(Window.width - player_img.width,
+#				Window.height - player_img.height,
+#				player_img)
+#end
+
+player = Sprite.new(400, 400, Image.load("./images/player.png"))
+
+Window.loop do
+	player.draw
+end
