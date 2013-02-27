1b0ce76b27bfc49adb434939026af04aedfb7dec
m-negishi <negileek22@gmail.com>
2013-02-27 14:07:29 +0900

    Do refactoring in main.rb and game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,10 @@
-﻿class Game
+﻿# coding: UTF-8
+
+require_relative 'map'
+require_relative 'water'
+require_relative 'user'
+
+class Game
   def initialize
     #@map = Map.new("sample1.dat")
   end
--- main.rb
+++ main.rb
@@ -2,14 +2,7 @@
 
 require 'dxruby'
 
-require_relative 'lib/map'
-require_relative 'lib/user'
-require_relative 'lib/water'
 require_relative 'lib/director'
-require_relative 'lib/title'
-require_relative 'lib/ending'
-require_relative 'lib/scene'
-require_relative 'lib/game'
 
 director = Director.new
 
