3f3eb6ab0e60cc6a1c7ceca0a8eb1f81caff2a23
m-negishi <negileek22@gmail.com>
2013-02-27 10:32:38 +0900

    Add require director.rb

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'dxruby'
 require_relative 'lib/map'
 require_relative 'lib/user'
 require_relative 'lib/water'
+require_relative 'lib/director'
 
 Window.loop do
 	break if Input.keyPush?(K_ESCAPE)
