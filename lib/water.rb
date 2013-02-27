5acd0c73904d10cd2912f429811d38eceab8682f
Kuwatani <fae3f5@gmail.com>
2013-02-27 17:30:02 +0900

    water_test

--- lib/water.rb
+++ lib/water.rb
@@ -1,21 +1,59 @@
-# coding utf-8
+# coding: utf-8
+require 'dxruby'
 
 class Water
-=begin
- 	def initialize( i_map ){
-	@map = []
-	@map  imap
-	}
-=end
-	map[
-	[1,0,1,1],
-	[1,0,1,1],
-	[0,0,0,0],
-	[0,1,1,0]
-	]
-	def move( x, y, value )
-		if()
+	
+ 	def initialize( tmp_i, tmp_j )
+		#@img = water_img
+		@map=[
+			[0,8,0,0],
+			[0,1,0,0],
+			[1,1,1,1],
+			[1,0,0,9]
+		]
+		@i = tmp_i
+		@j = tmp_j
+
+		@last_points=[[tmp_i, tmp_j]]
+
+		@map[@i][@j]=-1
+	end
+	
+	def move
+		# [ [x1, y1], [x2, y2], ...]
+		old_points = @last_points.dup
+		@last_points.clear
+		old_points.each do |(x, y)|
+			if x - 1 >= 0 && @map[x - 1][y] > 0
+				@map[x - 1][y] = -1
+				@last_points << [x - 1, y]
+			end
+
+			if x + 1 < 4 && @map[x + 1][y] > 0
+				@map[x + 1][y] = -1
+				@last_points << [x + 1, y]
+			end
+
+			if y - 1 >= 0 && @map[x][y-1] > 0
+				@map[x][y-1] = -1
+				@last_points << [x, y-1]
+			end
+
+			if y + 1 < 4 && @map[x][y+1] > 0
+				@map[x][y+1] = -1
+				@last_points << [x, y+1]
+			end
+		end
+
 	end
 
- 	end
+	def draw
+		4.times{ |ii|
+			4.times{ |jj|
+				if(@map[ii][jj]==-1)
+					Window.draw(jj*32,ii*32,Image.new(32,32,[0,0,255]))
+				end
+			}
+		}
+	end
 end
