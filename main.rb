ee03a26007cd0336beabf34631cb286ef382a484
m-negishi <negileek22@gmail.com>
2013-02-27 14:33:52 +0900

    fix conflict in main.rb

--- main.rb
+++ main.rb
@@ -1,21 +1,20 @@
 # coding: UTF-8
 
 require 'dxruby'
-
 require_relative 'lib/director'
 
-<<<<<<< HEAD
 director = Director.new
 
 Window.loop do
+	
 	break if Input.keyPush?(K_ESCAPE)
 	
 	director.play
-=======
-@map = Map.new("images/map01.dat")
+	@map = Map.new("images/map01.dat")
 
 Window.loop do
+	
 	break if Input.keyPush?(K_ESCAPE)
 	@map.draw
->>>>>>> kurumada
+	
 end
