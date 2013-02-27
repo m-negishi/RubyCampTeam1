85054309cd60fb3e4ad7111af7028a464ab22650
Miyawa <masaki-ai-aya@mrg.biglobe.ne.jp>
2013-02-27 14:47:39 +0900

    temp

--- main.rb
+++ main.rb
@@ -1,21 +1,12 @@
 # coding: UTF-8
 
 require 'dxruby'
-
 require_relative 'lib/director'
 
-<<<<<<< HEAD
 director = Director.new
 
 Window.loop do
-	break if Input.keyPush?(K_ESCAPE)
-	
+	break if Input.keyPush?(K_ESCAPE)	
 	director.play
-=======
-@map = Map.new("images/map01.dat")
-
-Window.loop do
-	break if Input.keyPush?(K_ESCAPE)
-	@map.draw
->>>>>>> kurumada
+	@map = Map.new("images/map01.dat")
 end
