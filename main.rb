7c031e0befff961aad925cb99cfc4793a661eafe
m-negishi <negileek22@gmail.com>
2013-02-27 11:06:56 +0900

    Add "Title","Ending" screen and that'images

--- main.rb
+++ main.rb
@@ -6,8 +6,20 @@ require_relative 'lib/map'
 require_relative 'lib/user'
 require_relative 'lib/water'
 require_relative 'lib/director'
+require_relative 'lib/title'
+require_relative 'lib/ending'
+require_relative 'lib/scene'
+
+title = Title.new
+ending = Ending.new
+
+Scene.add_scene(:title,  title)
+Scene.add_scene(:ending,  ending)
+
+Scene.set_scene(:title)
 
 Window.loop do
-	break if Input.keyPush?(K_ESCAPE)
+	break if Input.keyPush?(K_ESCAPE) || ending.closed
 	
+	Scene.play
 end
