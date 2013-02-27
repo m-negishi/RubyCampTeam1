6575e314bb13ad073f2f5b06375891b9e64fd70d
kairiyanagida <bmlt.wwgi@gmail.com>
2013-02-27 14:36:14 +0900

    collision test lib/user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,29 @@
-# coding: Shiht_JIS
+# coding: UTF-8
 
+class User < Sprite
+  def update
+    @dx = Input.x * 2
+    @dy = Input.y * 2
+    self.x += @dx
+    self.y += @dy
+  end
+
+  def hit(obj)
+    self.x -= @dx
+    self.y -= @dy
+  end
+
+	def shot(obj)
+		
+  end
+
+end
+
+class Wall < Sprite
+end
+
+class Pipe < Sprite
+end
+
+class Water < Sprite
+end
