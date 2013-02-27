360dfd4c613be1cdf8346925c5ce095e49e79ef5
m-negishi <negileek22@gmail.com>
2013-02-27 11:35:23 +0900

    Chanege parameter name

--- lib/ending.rb
+++ lib/ending.rb
@@ -2,11 +2,11 @@
   attr_reader :closed
 
   def initialize
-    @bg_img = Image.load("images/ending.png")
+    @background_img = Image.load("images/ending.png")
   end
 
   def play
     @closed = true if Input.keyPush?(K_SPACE)
-    Window.draw(0, 0, @bg_img)
+    Window.draw(0, 0, @background_img)
   end
 end
