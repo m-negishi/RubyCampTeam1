55f41c2ed62949f20228dc96c01941967c682172
m-negishi <negileek22@gmail.com>
2013-02-27 14:38:18 +0900

    fix conflict again in main.rb

--- main.rb
+++ main.rb
@@ -6,15 +6,7 @@ require_relative 'lib/director'
 director = Director.new
 
 Window.loop do
-	
-	break if Input.keyPush?(K_ESCAPE)
-	
+	break if Input.keyPush?(K_ESCAPE)	
 	director.play
 	@map = Map.new("images/map01.dat")
-
-Window.loop do
-	
-	break if Input.keyPush?(K_ESCAPE)
-	@map.draw
-	
 end
