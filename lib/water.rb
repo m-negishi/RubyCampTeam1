42f0a2d86ee88cd86b9d4b100114986fe2a44449
Kuwatani <fae3f5@gmail.com>
2013-02-27 17:53:33 +0900

    water_test

--- lib/water.rb
+++ lib/water.rb
@@ -4,7 +4,6 @@ require 'dxruby'
 class Water
 	
  	def initialize( tmp_i, tmp_j )
-		#@img = water_img
 		@map=[
 			[0,8,0,0],
 			[0,1,0,0],
@@ -17,36 +16,41 @@ class Water
 		@last_points=[[tmp_i, tmp_j]]
 
 		@map[@i][@j]=-1
+		@t=0
 	end
 	
 	def move
-		# [ [x1, y1], [x2, y2], ...]
-		old_points = @last_points.dup
-		@last_points.clear
-		old_points.each do |(x, y)|
-			if x - 1 >= 0 && @map[x - 1][y] > 0
-				@map[x - 1][y] = -1
-				@last_points << [x - 1, y]
-			end
+		#…‚Ìæ’[‚ªˆÚ“®
+		#Z…‚µ‚½ê‡‚Í-1‚É‚·‚é
+		if(@t % 60 == 0 && @t > 0)
+			old_points = @last_points.dup
+			@last_points.clear
+			old_points.each do |(x, y)|
+				if x - 1 >= 0 && @map[x - 1][y] > 0
+					@map[x - 1][y] = -1
+					@last_points << [x - 1, y]
+				end
 
-			if x + 1 < 4 && @map[x + 1][y] > 0
-				@map[x + 1][y] = -1
-				@last_points << [x + 1, y]
-			end
+				if x + 1 < 4 && @map[x + 1][y] > 0
+					@map[x + 1][y] = -1
+					@last_points << [x + 1, y]
+				end
 
-			if y - 1 >= 0 && @map[x][y-1] > 0
-				@map[x][y-1] = -1
-				@last_points << [x, y-1]
-			end
+				if y - 1 >= 0 && @map[x][y-1] > 0
+					@map[x][y-1] = -1
+					@last_points << [x, y-1]
+				end
 
-			if y + 1 < 4 && @map[x][y+1] > 0
-				@map[x][y+1] = -1
-				@last_points << [x, y+1]
+				if y + 1 < 4 && @map[x][y+1] > 0
+					@map[x][y+1] = -1
+					@last_points << [x, y+1]
+				end
 			end
 		end
-
+		@t+=1
 	end
-
+	
+	#Z…‰ÓŠ‚Ì•`‰æ
 	def draw
 		4.times{ |ii|
 			4.times{ |jj|
