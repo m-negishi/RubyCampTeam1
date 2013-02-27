7d6105b73617f9d66f9c156dd31d586a50e0586f
Kuwatani <fae3f5@gmail.com>
2013-02-27 15:06:06 +0900

    start to develop water

---
+++ lib/water.rb
@@ -0,0 +1,21 @@
+# coding utf-8
+
+class Water
+=begin
+ 	def initialize( i_map ){
+	@map = []
+	@map  imap
+	}
+=end
+	map[
+	[1,0,1,1],
+	[1,0,1,1],
+	[0,0,0,0],
+	[0,1,1,0]
+	]
+	def move( x, y, value )
+		if()
+	end
+
+ 	end
+end
